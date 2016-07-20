9eb213e33d31e07a7e504240326d13f88e84c3ba
Ken Grimm <ken.grimm@welltok.com>
2016-07-20 07:56:10 -0600

    Mouse creates ray, endPoint of ray draws sphere

---
+++ Assets/Scripts/InitializeGrid.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitializeGrid : MonoBehaviour {
+	private float MIN_RANGE = -3f;
+	private float MAX_RANGE = 3f;
+	private float INCREMENT = 0.5f;
+
+	private GameObject gridBlock;
+
+	void Start () {
+		// gridBlock = GameObject.Find("Grid Block");
+
+		// for(float x = MIN_RANGE; x < MAX_RANGE; x+= INCREMENT) {
+		// 	for(float y = MIN_RANGE; y < MAX_RANGE; y+= INCREMENT) {
+		// 		for(float z = MIN_RANGE; z < MAX_RANGE; z+= INCREMENT) {
+		// 			Instantiate(gridBlock, new Vector3(x, y, z), Quaternion.identity);
+		// 		}
+		// 	}
+		// }
+	}
+
+}
---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pointer : MonoBehaviour {
+	private float POINTER_LENGTH = 5;
+	private LineRenderer line;
+	private Dictionary<Vector3, bool> endPoints;
+	
+	public GameObject dot;
+	public float pointerLength;
+
+	void Start() {
+		endPoints = new Dictionary<Vector3, bool>();
+		line = GetComponent<LineRenderer>();
+		line.SetPosition(0, Camera.main.transform.position + new Vector3(0, -0.1f, 0));
+	}
+
+	void Update () {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		// Vector3 pointerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + (Camera.main.transform.forward * POINTER_LENGTH));
+		// Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+		
+		Vector3 endPoint = ray.origin + ray.direction * pointerLength;
+		
+		line.SetPosition(1, endPoint);
+
+		if(!endPoints.ContainsKey(endPoint)) {
+			endPoints[endPoint] = true;
+
+			Instantiate(dot, endPoint, Quaternion.identity);
+		}
+		
+		// Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition + (Vector3.forward * POINTER_LENGTH)));
+		// Debug.Log(pointerPos);
+		// Debug.DrawLine(Camera.main.transform.position, pointerPos);
+
+		
+
+		// if(Physics.Raycast(ray, out hit)) {
+		// 	Debug.Log(hit.transform.name);
+		// }
+	}
+}
