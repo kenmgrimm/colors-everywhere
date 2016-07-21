aad8218a4c5cb3b1fac3788afa59d28c6d8d1cc6
Ken Grimm <ken.grimm@welltok.com>
2016-07-21 08:05:56 -0600

    Added buttons for paint and color change

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+	public GameObject pointer;
+
+	void OnGUI () {
+		if(GUI.Button(new Rect(20,40,80,20), "Paint")) {
+			pointer.GetComponent<Pointer>().Paint();
+		}
+
+		if(GUI.Button(new Rect(20,60,80,20), "Color")) {
+			pointer.GetComponent<Pointer>().ChangeColor();
+		}
+	}
+	
+}
--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -8,6 +8,9 @@ public class Pointer : MonoBehaviour {
 	private Dictionary<Vector3, bool> endPoints;
 	private Camera augmentedCamera;
 
+	private Renderer rend;
+	private Color lerpedColor;
+
 	public GameObject dot;
 	public float pointerLength;
 
@@ -16,12 +19,19 @@ public class Pointer : MonoBehaviour {
 		endPoints = new Dictionary<Vector3, bool>();
 		line = GetComponent<LineRenderer>();
 		line.SetPosition(0, Camera.main.transform.position + new Vector3(0, -0.1f, 0));
+		
+    rend = GameObject.Find("Painted Dot Yellow").GetComponent<Renderer>();
 	}
 
-	void Update () {
+	public void ChangeColor() {
+		lerpedColor = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time, 1));
+		rend.material.color = lerpedColor;
+	}
+
+	public void Paint() {
 		RaycastHit hit;
 		// Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+		Ray ray = augmentedCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 		// Vector3 pointerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + (Camera.main.transform.forward * POINTER_LENGTH));
 		// Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
 		
@@ -34,15 +44,11 @@ public class Pointer : MonoBehaviour {
 
 			Instantiate(dot, endPoint, Quaternion.identity);
 		}
-		
-		// Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition + (Vector3.forward * POINTER_LENGTH)));
-		// Debug.Log(pointerPos);
-		// Debug.DrawLine(Camera.main.transform.position, pointerPos);
-
-		
-
 		// if(Physics.Raycast(ray, out hit)) {
 		// 	Debug.Log(hit.transform.name);
 		// }
+
 	}
+
+	void Update () {}
 }
