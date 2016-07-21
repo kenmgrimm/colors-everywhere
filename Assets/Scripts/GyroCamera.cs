ba1074df3d59f90ed9794c3dd7c4fd7f440fb447
Ken Grimm <ken.grimm@welltok.com>
2016-07-20 23:16:40 -0600

    Added Augmented Camera to display game objects.  On play now shows camera background with augmented objects overlayed

---
+++ Assets/Scripts/GyroCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class GyroCamera : MonoBehaviour {
+    Quaternion initialRotation;
+    Quaternion gyroInitialRotation;
+    bool gyroEnabled;
+    public float mouseSpeed = 1.0f;
+ 
+    void Start () {
+        Debug.Log("Start");
+        Input.gyro.enabled = true;
+
+        Invoke("BeginTracking", 2f);
+    }
+
+    void Update() {
+        // if(gyroEnabled){
+        // #if !UNITY_EDITOR
+            Quaternion offsetRotation = ConvertRotation(Quaternion.Inverse(gyroInitialRotation) * Input.gyro.attitude);
+            transform.rotation = initialRotation * offsetRotation;
+        // #else
+        //     transform.Rotate(Input.GetAxis("Mouse Y") * mouseSpeed, Input.GetAxis("Mouse X") * mouseSpeed, 0);
+        // #endif
+        // }
+    }
+
+    private void BeginTracking() {
+        Debug.Log("BeginTracking");
+        gyroEnabled = true;
+        initialRotation = transform.rotation;
+        gyroInitialRotation = Input.gyro.attitude;
+
+        // transform.rotation = initialRotation = gyroInitialRotation;
+    }
+ 
+    private static Quaternion ConvertRotation(Quaternion q) {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);  
+    }
+}
--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -6,11 +6,13 @@ public class Pointer : MonoBehaviour {
 	private float POINTER_LENGTH = 5;
 	private LineRenderer line;
 	private Dictionary<Vector3, bool> endPoints;
-	
+	private Camera augmentedCamera;
+
 	public GameObject dot;
 	public float pointerLength;
 
 	void Start() {
+		augmentedCamera = GameObject.Find("Augmented Camera").GetComponent<Camera>();
 		endPoints = new Dictionary<Vector3, bool>();
 		line = GetComponent<LineRenderer>();
 		line.SetPosition(0, Camera.main.transform.position + new Vector3(0, -0.1f, 0));
@@ -18,7 +20,8 @@ public class Pointer : MonoBehaviour {
 
 	void Update () {
 		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		// Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 		// Vector3 pointerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition + (Camera.main.transform.forward * POINTER_LENGTH));
 		// Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
 		
