34329d358bb3644f61c42d389020c188717b4f96
Ken Grimm <ken.grimm@welltok.com>
2016-07-22 11:38:45 -0600

    Got gyro initialization to work on Unity Remote

--- Assets/Scripts/GyroCamera.cs
+++ Assets/Scripts/GyroCamera.cs
@@ -2,48 +2,49 @@
 using System.Collections;
 
 public class GyroCamera : MonoBehaviour {
-    Quaternion initialRotation;
-    Quaternion gyroInitialRotation;
-    bool gyroEnabled;
-    public float mouseSpeed = 1.0f;
+  Quaternion initialRotation;
+  Quaternion gyroInitialRotation;
 
+  private bool gyroInitialized = false;
+  public float mouseSpeed = 1.0f;
 
-    #if UNITY_EDITOR
-      private bool HasGyro() { 
-        return UnityEditor.EditorApplication.isRemoteConnected;
-      }
-    #else
-      private bool HasGyro() { return true; }
-    #endif
-
-
-    void Start () {
-      Debug.Log("Start");
-
-      Invoke("BeginTracking", 2f);
+  #if UNITY_EDITOR
+    private bool HasGyro() { 
+      return UnityEditor.EditorApplication.isRemoteConnected;
     }
+  #else
+    private bool HasGyro() { return true; }
+  #endif
 
-    void Update() {
-      if(HasGyro()) {
-        Quaternion offsetRotation = ConvertRotation(Quaternion.Inverse(gyroInitialRotation) * Input.gyro.attitude);
-        transform.rotation = initialRotation * offsetRotation;
-      }
-      else {
-        Debug.Log("NO GYRO");
-        transform.Rotate(Input.GetAxis("Mouse Y") * mouseSpeed, Input.GetAxis("Mouse X") * mouseSpeed, 0);
-      }
+
+  void Start () {
+    // Not sure if necessary, doesn't appear to be
+    Input.gyro.enabled = true;
+
+    InvokeRepeating("InitializeGyro", 0, 1);
+  }
+
+  void InitializeGyro() {
+    if(HasGyro()) {
+      gyroInitialRotation = Input.gyro.attitude;
+      initialRotation = transform.rotation;
+
+      gyroInitialized = true;
+      CancelInvoke("InitializeGyro");
     }
+  }
 
-    private void BeginTracking() {
-        Debug.Log("BeginTracking");
-        gyroEnabled = true;
-        initialRotation = transform.rotation;
-        gyroInitialRotation = Input.gyro.attitude;
-
-        // transform.rotation = initialRotation = gyroInitialRotation;
+  void Update() {
+    if(HasGyro() && gyroInitialized) {
+      Quaternion offsetRotation = ConvertRotation(Quaternion.Inverse(gyroInitialRotation) * Input.gyro.attitude);
+      transform.rotation = initialRotation * offsetRotation;
     }
-
-    private static Quaternion ConvertRotation(Quaternion q) {
-        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    else {
+      transform.Rotate(Input.GetAxis("Mouse Y") * mouseSpeed, Input.GetAxis("Mouse X") * mouseSpeed, 0);
     }
+  }
+
+  private static Quaternion ConvertRotation(Quaternion q) {
+    return new Quaternion(q.x, q.y, -q.z, -q.w);
+  }
 }
