de1706215cde269f28fd19a435094433b771ac99
Ken Grimm <ken.grimm@welltok.com>
2016-07-28 15:56:09 -0600

    Added placeholder webcam images

---
+++ Assets/ReplaceWebcamImage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaceWebcamImage : MonoBehaviour {
+	public Material cameraPlaneMat;
+
+	void Start() {
+		Material[] materials = new Material[1];
+		materials[0] = cameraPlaneMat;
+		GetComponent<Renderer>().materials = materials;
+	}
+}
