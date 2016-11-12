01b9d4e0a38b42456362b4f2b73c22826325dde8
Ken Grimm <ken.grimm@welltok.com>
2016-11-12 15:03:19 -0700

    Cardboard scene now loads painting scene.  Shows painting scene environment when running in dev mode directly from cardboard scene.

---
+++ Assets/BootstrapCardboard.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class BootstrapCardboard : MonoBehaviour {
+	void Awake () {
+		if(GameObject.Find("Painting(Clone)")) {
+			GameObject.Find("Environment - Dev only").SetActive(false);
+		}
+	}
+}
