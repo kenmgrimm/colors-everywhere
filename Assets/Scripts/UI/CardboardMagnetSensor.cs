dcafd641652ca60a73ec512c9e219114f5e0f054
Ken Grimm <ken.grimm@welltok.com>
2016-11-13 19:32:03 -0700

    Close enough cardboard magnetic button using change in compass degrees > some threshold

--- Assets/Scripts/UI/CardboardMagnetSensor.cs
+++ Assets/Scripts/UI/CardboardMagnetSensor.cs
@@ -1,123 +1,43 @@
-﻿// From:  http://www.andrewnoske.com/wiki/Unity_-_Detecting_Google_Cardboard_Click
-
 using UnityEngine;
-using System.Collections.Generic;
+  
+public class CardboardMagnetSensor : MonoBehaviour {
+  private Vector3 lastCompass;
+ 
+  public float COMPASS_CHANGE_THRESHOLD = 30.0f;
+  public Vector3 compass;
 
-public class CardboardMagnetSensor {
-    // Constants:
-    private const int WINDOW_SIZE = 40;
-    private const int NUM_SEGMENTS = 2;
-    private const int SEGMENT_SIZE = WINDOW_SIZE / NUM_SEGMENTS;
-    private const int T1 = 30, T2 = 130;
+  private Vector3 Compass() {
+    #if UNITY_EDITOR
+      return compass;
+    #else
+      return Input.compass.rawVector;
+    #endif
+  }
 
-    // Variables:
-    private static bool wasClicked;           // Flips to true once set off.
-    private static bool sensorEnabled;        // Is sensor active.
-    private static List<Vector3> sensorData;  // Keeps magnetic sensor data.
-    private static float[] offsets;           // Offsets used to detect click.
-
-
-    // Call this once at beginning to enable detection.
-    public static void SetEnabled(bool enabled) {
-      Reset();
-      sensorEnabled = enabled;
-      Input.compass.enabled = sensorEnabled;
+  void Update() {
+    Vector3 current = Compass();
+    if(LargeChange(lastCompass, current)) {
+      Debug.Log("Button Pressed: " + lastCompass + ", " + current);
     }
 
-    // Reset variables.
-    public static void Reset() {
-      sensorData = new List<Vector3>(WINDOW_SIZE);
-      offsets = new float[SEGMENT_SIZE];
-      wasClicked = false;
-      sensorEnabled = false;
+    lastCompass = current;
+  }
+
+  void Awake() {
+    Input.compass.enabled = true;
+
+    lastCompass = Compass();
+
+    // tiltedMagn = Input.acceleration.magnitude;
+  }
+ 
+  private bool LargeChange(Vector3 prev, Vector3 current) {
+    if (Mathf.Abs(prev.x - current.x) > COMPASS_CHANGE_THRESHOLD ||
+      Mathf.Abs(prev.y - current.y) > COMPASS_CHANGE_THRESHOLD ||
+      Mathf.Abs(prev.z - current.z) > COMPASS_CHANGE_THRESHOLD) {
+      return true;
     }
 
-    // Poll this once every frame to detect when the magnet button was clicked
-    // and if it was clicked make sure to call "ResetClick()"
-    // after you've dealt with the action, or it will continue to return true.
-    public static bool CheckIfWasClicked() {
-        UpdateData();
-        return wasClicked;
-    }
-
-    // Call this after you've dealt with a click operation.
-    public static void ResetClick() {
-        wasClicked = false;
-    }
-
-    // Updates 'sensorData' and determines if magnet was clicked.
-    private static void UpdateData() {
-        Vector3 currentVector = Input.compass.rawVector;
-
-
-
-        if (currentVector.x == 0 && currentVector.y == 0 && currentVector.z == 0) {
-
-            return;
-        }
-
-        if(sensorData.Count >= WINDOW_SIZE) sensorData.RemoveAt(0);
-        sensorData.Add(currentVector);
-
-        // Evaluate model:
-        if(sensorData.Count < WINDOW_SIZE) return;
-
-        float[] means = new float[2];
-        float[] maximums = new float[2];
-        float[] minimums = new float[2];
-
-        Vector3 baseline = sensorData[sensorData.Count - 1];
-
-        for(int i = 0; i < NUM_SEGMENTS; i++) {
-            int segmentStart = 20 * i;
-            offsets = ComputeOffsets(segmentStart, baseline);
-
-            means[i] = ComputeMean(offsets);
-            maximums[i] = ComputeMaximum(offsets);
-            minimums[i] = ComputeMinimum(offsets);
-        }
-
-        float min1 = minimums[0];
-        float max2 = maximums[1];
-
-        // Determine if button was clicked.
-        if(min1 < T1 && max2 > T2) {
-            sensorData.Clear();
-            wasClicked = true;  // Set button clicked to true.
-            // NOTE: 'wasClicked' will now remain true until "ResetClick()" is called.
-        }
-    }
-
-    private static float[] ComputeOffsets(int start, Vector3 baseline) {
-        for(int i = 0; i < SEGMENT_SIZE; i++) {
-            Vector3 point = sensorData[start + i];
-            Vector3 o = new Vector3(point.x - baseline.x, point.y - baseline.y, point.z - baseline.z);
-            offsets[i] = o.magnitude;
-        }
-        return offsets;
-    }
-
-    private static float ComputeMean(float[] offsets) {
-        float sum = 0;
-        foreach(float o in offsets) {
-            sum += o;
-        }
-        return sum / offsets.Length;
-    }
-
-    private static float ComputeMaximum(float[] offsets) {
-        float max = float.MinValue;
-        foreach(float o in offsets) {
-            max = Mathf.Max(o, max);
-        }
-        return max;
-    }
-
-    private static float ComputeMinimum(float[] offsets) {
-        float min = float.MaxValue;
-        foreach(float o in offsets) {
-            min = Mathf.Min(o, min);
-        }
-        return min;
-    }
+    return false;
+  }
 }
--- Assets/Scripts/UI/MagnetButton.cs
+++ Assets/Scripts/UI/MagnetButton.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 
 public class MagnetButton : MonoBehaviour {
-	private int MAGNET_STRENGTH = 600;
+	// private int MAGNET_STRENGTH = 600;
 	
-	private bool wasDown = false;
+	// private bool wasDown = false;
+	// private CardboardMagnetSensor sensor;
 
 	public delegate void ClickHandler();
   public event ClickHandler OnButtonClick;
 
-	void Start() {
+	void Update() {
+		// sensor.MagUpdate(Input.acceleration, Input.compass.rawVector);
+		// Debug.Log("Click: " + Input.acceleration + ", " + Input.compass.rawVector + ", " + sensor.Clicked());
+		Debug.Log(Input.compass.rawVector.x + ", " + Input.compass.rawVector.y + ", " + Input.compass.rawVector.z);
+	}
+
+	void Awake() {
+		// sensor = new CardboardMagnetSensor();
 		Debug.Log("Input.compass.rawVector");
 
 		// OnButtonClick += LogClick;
@@ -17,19 +25,19 @@ public class MagnetButton : MonoBehaviour {
 		// Input.location.Start();
 		
 		Input.compass.enabled = true;
-		CardboardMagnetSensor.SetEnabled(true);
+		// CardboardMagnetSensor.SetEnabled(true);
 
-		InvokeRepeating("CheckButton", 0, 0.1f);
+		// InvokeRepeating("CheckButton", 0, 0.1f);
 	}
 
-	private void CheckButton() {
-		Debug.Log(Input.compass.rawVector);
+	// private void CheckButton() {
+	// 	Debug.Log(Input.compass.rawVector);
 
-		if (CardboardMagnetSensor.CheckIfWasClicked()) {
-			Debug.Log("Cardboard trigger was just clicked");
+	// 	if (CardboardMagnetSensor.CheckIfWasClicked()) {
+	// 		Debug.Log("Cardboard trigger was just clicked");
 
-			CardboardMagnetSensor.ResetClick();
-		}
+	// 		CardboardMagnetSensor.ResetClick();
+	// 	}
 
 		// if(IsDown()) {
 		// 	wasDown = true;
@@ -40,5 +48,5 @@ public class MagnetButton : MonoBehaviour {
 		// 	Debug.Log("WasDown but isn't down: " + Input.compass.rawVector);
 		// 	OnButtonClick();
 		// }
-	}
+	// }
 }
